4a2ad00b8f65aeeb827bf43dd9360a10be503cb3
khai520 <khai5108@gmail.com>
2024-05-25 11:14:56 +0700

    new

--- DuAn1/MainApp/Program.cs
+++ DuAn1/MainApp/Program.cs
@@ -13,7 +13,7 @@ namespace MainApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new BanHang());
+            Application.Run(new Login());
         }
     }
 }
